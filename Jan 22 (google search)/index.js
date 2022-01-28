99548550269e984ed263a408271b050515f2aef9
sachinpundir1 <sachin.pundir.431@gmail.com>
2022-01-28 22:29:49 +0530

    google search

---
+++ Jan 22 (google search)/index.js
@@ -0,0 +1,20 @@
+const searchBar = document.getElementById('search-bar');
+
+searchBar.addEventListener('keypress', function(event){
+    if(event.key === "Enter"){
+        console.log('searched');
+    }
+})
+
+
+// set searched data to the cookies
+const setCookieData = () => {
+    
+}
+
+// get cookies data for suggestions
+const getSuggestions = () => {
+
+}
+
+
